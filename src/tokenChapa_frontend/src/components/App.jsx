91cad82ba42f4af0392af0f9410d254c84caf8a9
Dedwison <dedwison@gmail.com>
2025-03-21 13:10:24 -0300

    Header component

---
+++ src/tokenChapa_frontend/src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { tokenChapa_backend } from 'declarations/tokenChapa_backend';
+
+function App() {
+
+  return (
+    <main>
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/tokenChapa_frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header>
+            <div className="blue window" id="logo">
+                <h1>
+                    <span role="img" aria-label="tap emoji">
+                        🪙
+                    </span>
+                    CHAPA Oferta Inicial
+                </h1>
+            </div>
+        </header>
+    )
+}
